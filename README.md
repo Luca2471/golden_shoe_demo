45ba6281ce340e1b9383edc5e1ef92470dcedbd5
Ross Cumming <rosso5792@gmail.com>
2020-10-05 22:23:16 +0100

    mongo run script working

---
+++ README.md
@@ -0,0 +1,13 @@
+## How to start
+
+CD into client folder and type the following in terminal:
+
+```
+npm install
+```
+
+CD into server folder and type the following in terminal:
+
+```
+npm install
+```
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,8 +1,11 @@
-[
+use products;
+db.dropDatabase();
+
+db.products.insertMany([
   {
     "brand": "Nike",
     "model": "Air Max",
-    "price": 120.0,
+    "price": 120,
     "colour": "Black",
     "size": [5, 6, 7, 8, 9, 10, 11, 12],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/nike-airmax-black-men.jpg",
@@ -12,7 +15,7 @@
   {
     "brand": "Nike",
     "model": "Air Max",
-    "price": 115.0,
+    "price": 115,
     "colour": "White",
     "size": [5, 6, 7, 8, 9, 10, 11, 12],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/nike-airmax-white-men.jpg",
@@ -22,7 +25,7 @@
   {
     "brand": "Adidas",
     "model": "Ultraboost",
-    "price": 160.0,
+    "price": 160,
     "colour": "Black",
     "size": [5, 6, 7, 8, 9, 10, 11, 12],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/adidas-ultraboost-black-men.jpg",
@@ -32,7 +35,7 @@
   {
     "brand": "Adidas",
     "model": "Running",
-    "price": 80.0,
+    "price": 80,
     "colour": "White",
     "size": [5, 6, 7, 8, 9, 10, 11, 12],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/adidas-running-white-men.jpg",
@@ -42,7 +45,7 @@
   {
     "brand": "Vans",
     "model": "Filmore Decon",
-    "price": 45.0,
+    "price": 45,
     "colour": "Peach",
     "size": [4, 5, 6, 7, 8],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/vans-filmore-decon-peach-women.jpg",
@@ -52,7 +55,7 @@
   {
     "brand": "London Rebel",
     "model": "Heeled Ankle Boots",
-    "price": 32.0,
+    "price": 32,
     "colour": "Black",
     "size": [4, 5, 6, 7, 8],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/london-rebel-heeled-ankle-boots-black-women.jpg",
@@ -62,7 +65,7 @@
   {
     "brand": "Depp",
     "model": "Flat Shoes",
-    "price": 46.0,
+    "price": 46,
     "colour": "White",
     "size": [4, 5, 6, 7, 8],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/depp-flat-shoes-white-women.jpg",
@@ -72,11 +75,11 @@
   {
     "brand": "New Look",
     "model": "Ankle Boots",
-    "price": 28.0,
+    "price": 28,
     "colour": "Brown",
     "size": [4, 5, 6, 7, 8],
     "img": "https://prjimgs.s3.eu-west-2.amazonaws.com/new-look-ankle-boot-brown-women.jpg",
-    "stock": 3,
+    "stock": 0,
     "gender": "Female"
   }
-]
+]);
