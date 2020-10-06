9405661e654f4de7ff174bc22232adc02d750007
Ross Cumming <rosso5792@gmail.com>
2020-10-06 11:57:00 +0100

    added delivery dates to shopping basket

--- client/src/components/ShoppingCart/ShoppingCart.js
+++ client/src/components/ShoppingCart/ShoppingCart.js
@@ -3,6 +3,20 @@ import "./ShoppingCart.scss";
 import Footer from "../Footer/Footer";
 
 const ShoppingCart = ({ cart, removeFromCart }) => {
+  const NextDayDeliveryDate = () => {
+    let tomorrow = new Date();
+    tomorrow.setDate(new Date().getDate() + 1);
+    return tomorrow.toDateString();
+  };
+
+  const StandardDeliveryDate = () => {
+    let currentDayThree = new Date();
+    let currentDayFive = new Date();
+    currentDayThree.setDate(new Date().getDate() + 3);
+    currentDayFive.setDate(new Date().getDate() + 5);
+    return `${currentDayThree.toDateString()} - ${currentDayFive.toDateString()}`;
+  };
+
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
   const costs = cart.map((product) => product.price);
@@ -30,6 +44,36 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
                   </div>
                 </div>
               ))}
+          </div>
+          <div className="delivery-container">
+            <div className="delivery-options">
+              <div className="delivery-input">
+                <input
+                  type="radio"
+                  value="Next Day Delivery"
+                  name="delivery"
+                ></input>
+                <label>Next Day Delivery</label>
+                {cart.length > 0 ? (
+                  <NextDayDeliveryDate />
+                ) : (
+                  "No items in basket"
+                )}
+              </div>
+              <div className="delivery-input">
+                <input
+                  type="radio"
+                  value="Standard Delivery"
+                  name="delivery"
+                ></input>
+                <label>Standard Delivery</label>
+                {cart.length > 0 ? (
+                  <StandardDeliveryDate />
+                ) : (
+                  "No items in basket"
+                )}
+              </div>
+            </div>
           </div>
         </div>
         <div className="cart-footer">
